3eb41086befde8f70095bfe67c63d91d50304862
chokri <chokribenmahjoub@gmail.com>
2024-02-25 21:33:51 +0100

    atelier 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-data-binding';
+  userList = [
+    {name:'Mohamed', age:10} ,
+    {name:'Ali', age:20} ,
+    {name:'Samia', age:12} ,
+    {name:'Saleh', age:25 },
+    ];
+    etat_bouton=true;
+
+    etat_affichage= false;
+    afficher()
+    {
+     this.etat_affichage=true;
+    }
+    toggleAffichage() {
+      this.etat_affichage = !this.etat_affichage;
+    }
+    //exercicie >>>> ajouter deux buttons
+
+    compteur = 0;
+
+  incrementerCompteur() {
+    if (this.compteur < 10) {
+      this.compteur++;
+    }
+  }
+
+  decrementerCompteur() {
+    if (this.compteur > 0) {
+      this.compteur--;
+    }
+  }
+ 
+
+
+
+
 }
